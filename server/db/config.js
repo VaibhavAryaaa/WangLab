77412512f27ad851015ccdb827af279ef3606f16
VaibhavAryaaa <aryavaibhav531@gmail.com>
2024-06-26 01:58:42 +0530

    something new

--- server/db/config.js
+++ server/db/config.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Wanglab"
+mongoose.connect("mongodb+srv://admin_nayan:nayan@proshop.pvjjtl3.mongodb.net/wangLab"
 );
