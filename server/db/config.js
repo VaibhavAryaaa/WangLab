ff1b88ca09f9c2322df2a351cf1e31a70cec030d
VaibhavAryaaa <aryavaibhav531@gmail.com>
2024-08-06 12:39:12 +0530

    small

--- server/db/config.js
+++ server/db/config.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://admin_vaibhav:4KoNAQJRyDJEAGQI@cluster0.i5i5wni.mongodb.net/Wanglab/"
+mongoose.connect("mongodb+srv://admin_vaibhav:PvI8E7TbfH6HKyuG@cluster0.i5i5wni.mongodb.net/Wanglab"
 );
